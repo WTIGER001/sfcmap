643b56d6280fc6dd2a47635bed9056a5537829d6
John Bauer <john.baueriii@gmail.com>
2018-07-15 22:51:04 -0400

    Fix build

--- src/app/controls/viewbox.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ViewboxDirective } from './viewbox.directive';
-
-describe('ViewboxDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ViewboxDirective();
-    expect(directive).toBeTruthy();
-  });
-});
