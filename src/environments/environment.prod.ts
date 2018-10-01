b25884dbbf03acdaded352e3a7d507896f2d23c7
John Bauer <john.baueriii@gmail.com>
2018-10-01 18:59:12 -0400

    updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,18 +1,18 @@
 export const environment = {
   production: true,
   firebase: {
-    // apiKey: "AIzaSyDtGtQCZWLD5FPEsJvbUp-HKp3ZqPIEXFA",
-    // authDomain: "sfcmap.firebaseapp.com",
-    // databaseURL: "https://sfcmap.firebaseio.com",
-    // projectId: "sfcmap",
-    // storageBucket: "sfcmap.appspot.com",
-    // messagingSenderId: "273084079541",
-    apiKey: "AIzaSyBd_fbJbbJPliH5LktUCfIevyHR82sprWY",
-    authDomain: "sfcmapdev.firebaseapp.com",
-    databaseURL: "https://sfcmapdev.firebaseio.com",
-    projectId: "sfcmapdev",
-    storageBucket: "sfcmapdev.appspot.com",
-    messagingSenderId: "885666068352"
+    apiKey: "AIzaSyDtGtQCZWLD5FPEsJvbUp-HKp3ZqPIEXFA",
+    authDomain: "sfcmap.firebaseapp.com",
+    databaseURL: "https://sfcmap.firebaseio.com",
+    projectId: "sfcmap",
+    storageBucket: "sfcmap.appspot.com",
+    messagingSenderId: "273084079541",
+    // apiKey: "AIzaSyBd_fbJbbJPliH5LktUCfIevyHR82sprWY",
+    // authDomain: "sfcmapdev.firebaseapp.com",
+    // databaseURL: "https://sfcmapdev.firebaseio.com",
+    // projectId: "sfcmapdev",
+    // storageBucket: "sfcmapdev.appspot.com",
+    // messagingSenderId: "885666068352"
   },
   logLevel: 'INFO',
   logCategories: [
